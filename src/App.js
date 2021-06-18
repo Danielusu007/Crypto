99066c6b834749ebb6992371f5700dd017fe3033
Daniel <rosenusu@gmail.com>
2021-06-18 15:02:47 +0200

    primeroC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Chart1 from './Chart1';
+import Chart2 from './Chart2';
+
+
+
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+
+      <header>
+        <h1>Crypto Cry</h1>
       </header>
-    </div>
+
+      <div className="contenedor">
+
+        <nav>
+
+          <div>
+            <p>bnb</p>
+          </div>
+
+          <div>
+            <span className="material-icons">&#xe8b6;</span>
+            <input></input>
+
+          </div>
+
+
+        </nav>
+
+        <section>
+
+
+
+
+          <Chart2 ></Chart2>
+
+
+
+        </section>
+
+        <footer>
+
+          <div>
+
+            <div className="contFechas">
+              <input></input>
+              <input></input>
+            </div>
+
+            <div className="contInversion">
+              <input></input>
+            </div>
+
+          </div>
+
+          <div className="contBeneficios" >
+            <p >beneficios</p>
+          </div>
+
+
+        </footer>
+      </div>
+    </>
   );
 }
+
+
+
+
 
 export default App;
